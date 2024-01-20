8198b9ccae6dd305d9f447d5613e4959038d554d
sourav-mahmud <souravmahmud32@gmail.com>
2024-01-21 03:26:09 +0600

    product data showed on the product card

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
 
 interface ProductCardProps{
@@ -8,6 +10,8 @@ interface ProductCardProps{
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({data}) => {
+    const productRating = data.reviews.reduce((acc: number, item: any) =>
+        item.rating + acc, 0) / data.reviews.length
     return ( 
         <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
             <div className=" flex flex-col items-center w-full gap-1">
@@ -17,9 +21,15 @@ const ProductCard: React.FC<ProductCardProps> = ({data}) => {
                 <div className="mt-4">
                     {truncateText(data.name)}
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <Rating value={productRating} readOnly></Rating>
+                </div>
+                <div>
+                    {data.reviews.length} reviews
+                </div>
+                <div className=" font-semibold">
+                    {formatPrice(data.price)}
+                </div>
             </div>
         </div>
      );
