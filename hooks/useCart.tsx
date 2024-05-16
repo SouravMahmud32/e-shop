f7b6ea489633a8440c57ccb40137bb5fc6069ddb
sourav-mahmud <souravmahmud32@gmail.com>
2024-05-16 18:28:14 +0600

    cart item quantity decreasing

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -14,6 +14,7 @@ type CartContextType = {
   handleAddProductToCart: (product: CartProductType) => void;
   handleRemoveProductFromCart: (product: CartProductType) => void;
   handleCartQtyIncrease: (product: CartProductType) => void;
+  handleCartQtyDecrease: (product: CartProductType) => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -84,12 +85,39 @@ export const CartContextProvider = (props: Props) => {
         );
 
         if (existingIndex > -1) {
-            updatedCart[existingIndex].quantity = ++updatedCart[existingIndex].quantity
+          updatedCart[existingIndex].quantity = ++updatedCart[existingIndex]
+            .quantity;
         }
 
-        setCartProducts(updatedCart)
-        localStorage.setItem('eShopCartItems', JSON.stringify(updatedCart))
-    
+        setCartProducts(updatedCart);
+        localStorage.setItem("eShopCartItems", JSON.stringify(updatedCart));
+      }
+    },
+    [cartProducts]
+  );
+
+
+  const handleCartQtyDecrease = useCallback(
+    (product: CartProductType) => {
+      let updatedCart;
+      if (product.quantity === 1) {
+        toast.error("Opss! Minimum reached!");
+      }
+
+      if (cartProducts) {
+        updatedCart = [...cartProducts];
+
+        const existingIndex = cartProducts.findIndex(
+          (item) => item.id === product.id
+        );
+
+        if (existingIndex > -1) {
+          updatedCart[existingIndex].quantity = --updatedCart[existingIndex]
+            .quantity;
+        }
+
+        setCartProducts(updatedCart);
+        localStorage.setItem("eShopCartItems", JSON.stringify(updatedCart));
       }
     },
     [cartProducts]
@@ -100,7 +128,8 @@ export const CartContextProvider = (props: Props) => {
     cartProducts,
     handleAddProductToCart,
     handleRemoveProductFromCart,
-    handleCartQtyIncrease
+    handleCartQtyIncrease,
+    handleCartQtyDecrease
   };
   return <CartContext.Provider value={value} {...props}></CartContext.Provider>;
 };
