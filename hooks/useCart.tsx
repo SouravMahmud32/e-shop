cc90931b88e1076fca56bf08d5f63a4697ec28a2
sourav-mahmud <souravmahmud32@gmail.com>
2024-05-16 15:06:02 +0600

    remove cart item

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -6,6 +6,7 @@ type CartContextType = {
     cartTotalQty: number;
     cartProducts: CartProductType[] | null;
     handleAddProductToCart: (product: CartProductType) => void;
+    handleRemoveProductFromCart: (product: CartProductType) => void;
 }
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -39,11 +40,26 @@ export const CartContextProvider = (props: Props) =>{
             localStorage.setItem('eShopCartItems', JSON.stringify(updatedCart))
             return updatedCart;
         })
-    }, [])
+    }, []);
+
+
+    const handleRemoveProductFromCart = useCallback((product: CartProductType) =>{
+        if (cartProducts) {
+            const filteredProducts = cartProducts.filter((item) =>{
+                item.id !== product.id
+            })
+
+            setCartProducts(filteredProducts)
+            toast.success('Product Removed')
+            localStorage.setItem('eShopCartItems', JSON.stringify(filteredProducts))
+        }
+    }, [cartProducts])
+
     const value = {
         cartTotalQty,
         cartProducts,
         handleAddProductToCart,
+        handleRemoveProductFromCart
     }
     return <CartContext.Provider value={value} {...props}></CartContext.Provider>
 };
