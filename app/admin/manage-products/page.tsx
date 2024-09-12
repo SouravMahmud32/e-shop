6a02b64c868f25aae65d56e9cc2001e9758eb84c
sourav-mahmud <souravmahmud32@gmail.com>
2024-09-12 17:39:04 +0600

    add data table in manage products

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,6 +1,21 @@
-const ManageProducts = () => {
+import Container from "@/app/components/Container";
+import ManageProductsClient from "./ManageProductsClient";
+import getProducts from "@/actions/getProducts";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const ManageProducts = async() => {
+    const products = await getProducts({category: null})
+    const currentUser = await getCurrentUser()
+
+    if (!currentUser || currentUser.role !== 'ADMIN') {
+        return <NullData title="Opps! Access Denied!"></NullData>
+      }
+
     return ( <div>
-        Manage Products
+        <Container>
+            <ManageProductsClient products={products}></ManageProductsClient>
+        </Container>
     </div> );
 }
  
