1e45ee4dd0f516f26a1a87ba8fdeabbc461bcf7c
sourav-mahmud <souravmahmud32@gmail.com>
2024-09-18 01:05:23 +0600

    show product from the database

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,35 @@ import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
 import { truncateText } from "@/utils/truncateText";
 import ProductCard from "./components/products/ProductCard";
+import getProducts, { IProductParams } from "@/actions/getProducts";
+import NullData from "./components/NullData";
 
-export default function Home() {
+
+interface HomeProps{
+  searchParams: IProductParams
+}
+
+export default async function Home({searchParams}: HomeProps) {
+  const products = await getProducts(searchParams)
+
+  if (products.length === 0) {
+    return <NullData title="Oops! No Products Found, click 'ALL' to clear filters "></NullData>
+  }
+
+  // suffle function products
+
+  function suffleArray(array: any){
+    for(let i = array.length - 1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i+1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+
+    return array
+
+  }
+
+  const suffleProducts = suffleArray(products)
+
   return (
     <div className="p-8">
       <Container>
@@ -12,7 +39,7 @@ export default function Home() {
           <HomeBanner></HomeBanner>
         </div>
         <div className=" grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) =>{
+          {suffleProducts.map((product: any) =>{
             return (
               <ProductCard data={product}></ProductCard>
             )
