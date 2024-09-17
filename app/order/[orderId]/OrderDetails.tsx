0162b0f7f02e19fdd8925dd301abc2526d1cade4
sourav-mahmud <souravmahmud32@gmail.com>
2024-09-17 17:19:36 +0600

    order items showed on orders details page

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -4,7 +4,9 @@ import Heading from "@/app/components/Heading";
 import Status from "@/app/components/Status";
 import { formatPrice } from "@/utils/formatPrice";
 import { Order } from "@prisma/client";
+import moment from "moment";
 import { MdAccessTimeFilled, MdDeliveryDining, MdDone } from "react-icons/md";
+import OrderItem from "./OrderItem";
 
 interface OrderDetailsProps {
   order: Order;
@@ -71,6 +73,20 @@ const OrderDetails: React.FC<OrderDetailsProps> = ({ order }) => {
             <></>
           )}
         </div>
+      </div>
+      <div>Date: {moment(order.createdAt).fromNow()}</div>
+      <div>
+        <h2 className=" font-semibold mt-4 mb-2">Products Ordered</h2>
+        <div className=" grid grid-cols-5 text-xs gap-4 pb-2 items-center">
+          <div className="col-span-2 justify-self-start">PRODUCT</div>
+          <div className="justify-self-center">PRICE</div>
+          <div className="justify-self-center">QTY</div>
+          <div className="justify-self-end">TOTAL AMOUNT</div>
+        </div>
+        {order.products &&
+          order.products.map((item) => {
+            return <OrderItem key={item.id} item={item}></OrderItem>;
+          })}
       </div>
     </div>
   );
